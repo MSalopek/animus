497ae248b1e11ffe57b1e32fd0bc4b05793460c9
Matija Salopek <matija.salopek@refurbed.com>
2022-07-12 13:35:55 +0200

    web: refactor sidebar icons

--- web/animus-ui/components/layout/Sidebar.jsx
+++ web/animus-ui/components/layout/Sidebar.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import Link from 'next/link';
+
 import { Menu } from '@headlessui/react';
 
-import { signOut } from "next-auth/react"
+import { signOut } from 'next-auth/react';
+
+import {
+  AdjustmentsIcon,
+  BookOpenIcon,
+  ChartBarIcon,
+  CreditCardIcon,
+  DatabaseIcon,
+  LogoutIcon,
+  // FingerPrintIcon,
+  // GlobeAltIcon,
+  // OfficeBuildingIcon,
+  TerminalIcon,
+  UserCircleIcon,
+} from '@heroicons/react/outline';
 
 export default function Sidebar() {
+  const [active, setActive] = useState(false);
+
   return (
     <aside className="h-screen sticky top-0 flex flex-col items-center w-60 text-gray-400 bg-gray-900">
       <a className="flex items-center text w-full pt-8 px-4" href="#">
@@ -13,7 +31,6 @@ export default function Sidebar() {
           xmlns="http://www.w3.org/2000/svg"
           className="w-10 h-10 fill-current"
         >
-          {/* <rect width="1024" height="1024" rx="221.867" fill="white" /> */}
           <path
             fillRule="evenodd"
             clipRule="evenodd"
@@ -51,128 +68,55 @@ export default function Sidebar() {
         {/* BASIC */}
         <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
           <a
-            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+              active ? 'bg-gray-700' : ''
+            } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
             href="#"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-              />
-            </svg>
+            <ChartBarIcon className="w-6 h-6" />
             <span className="ml-2 text-sm font-medium">Dashboard</span>
           </a>
-          <Link href="/storage/manager">
-            <a className="flex items-center w-full h-12 px-3 mt-2 rounded bg-gray-700 hover:bg-gray-700 text-gray-200 hover:text-gray-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
-                />
-              </svg>
+          <Link href="/">
+            <a
+              className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+                active ? 'bg-gray-700' : ''
+              } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
+            >
+              <DatabaseIcon className="w-6 h-6" />
               <span className="ml-2 text-sm font-medium">Storage</span>
             </a>
           </Link>
-          <a
+          {/* <a
             className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
             href="#"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-              />
-            </svg>
+            <GlobeAltIcon className="w-6 h-6"/>
             <span className="ml-2 text-sm font-medium">Gateways</span>
           </a>
           <a
             className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
             href="#"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-              />
-            </svg>
+            <FingerPrintIcon className="w-6 h-6"/>
             <span className="ml-2 text-sm font-medium">CID Tools</span>
-          </a>
+          </a> */}
         </div>
         {/* ADVANCED */}
         <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
-          <a
-            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-            href="#"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-            <span className="ml-2 text-sm font-medium">Automation</span>
-          </a>
           <Link href="/account/keys">
-            <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01"
-                />
-              </svg>
+            <a
+              className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+                active ? 'bg-gray-700' : ''
+              } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
+            >
+              <TerminalIcon className="w-6 h-6" />
               <span className="ml-2 text-sm font-medium">API Access</span>
             </a>
           </Link>
           <a
-            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+              active ? 'bg-gray-700' : ''
+            } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
             href="#"
           >
             <svg
@@ -189,49 +133,35 @@ export default function Sidebar() {
                 d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
               />
             </svg>
-            <span className="ml-2 text-sm font-medium">Private Networks</span>
+            <span className="ml-2 text-sm font-medium">Security</span>
           </a>
+          <Link href="/">
+            <a
+              className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+                active ? 'bg-gray-700' : ''
+              } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
+            >
+              <CreditCardIcon className="w-6 h-6" />
+              <span className="ml-2 text-sm font-medium">Billing</span>
+            </a>
+          </Link>
         </div>
         {/* DOCS SECTION */}
         <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
-          <a
+          {/* <a
             className="flex items-center w-full h-12 px-3 mt-2 rounded"
             href="#"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
-              />
-            </svg>
+            <OfficeBuildingIcon className="w-6 h-6" />
             <span className="ml-2 text-sm font-medium">Organization</span>
-          </a>
+          </a> */}
           <a
-            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            className={`flex items-center w-full h-12 px-3 mt-2 rounded ${
+              active ? 'bg-gray-700' : ''
+            } hover:bg-gray-700 text-gray-200 hover:text-gray-300`}
             href="#"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-              />
-            </svg>
+            <BookOpenIcon className="w-6 h-6" />
             <span className="ml-2 text-sm font-medium">Documentation</span>
           </a>
         </div>
@@ -250,20 +180,7 @@ function AccountMenu() {
             aria-label="account menu"
             className="flex items-center w-full dark:text-gray-200 hover:text-gray-50 dark:hover:text-gray-400 focus:text-gray-50 dark:focus:text-gray-400 focus:outline-none"
           >
-            <svg
-              className="w-6 h-6 stroke-current"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <UserCircleIcon className="w-6 h-6" />
             <span className="text-sm font-medium p-2">Account</span>
           </Menu.Button>
         </div>
@@ -277,77 +194,8 @@ function AccountMenu() {
                       active ? 'bg-blue-500 text-white' : 'text-gray-900'
                     } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="gray"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                      />
-                    </svg>
+                    <AdjustmentsIcon className="w-6 h-6" />
                     <span className="px-2 font-semi">Settings</span>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
-          </div>
-          <div className="px-1 py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/settings/billing">
-                  <a
-                    className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="green"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                      />
-                    </svg>
-                    <span className="px-2 font-semi">Billing</span>
-                  </a>
-                </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <Link href="/settings/keys">
-                  <a
-                    className={`${
-                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="gray"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
-                      />
-                    </svg>
-                    <span className="px-2 font-semi">API Access</span>
                   </a>
                 </Link>
               )}
@@ -362,20 +210,7 @@ function AccountMenu() {
                   } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
                   onClick={() => signOut()}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="fuchsia"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                    />
-                  </svg>
+                  <LogoutIcon className="w-5 h-5 text-rose-600" />
                   <span className="px-1">Sign Out</span>
                 </button>
               )}
